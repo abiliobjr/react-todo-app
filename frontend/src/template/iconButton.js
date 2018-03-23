6eeb957ba3eba3d16c9c01a1eb0d7bc41c56a243
Abilio Bittencourt Junior <abilio.junior@b2wdigital.com>
2018-03-23 13:40:27 -0300

    Mudando condicional para ternário

--- frontend/src/template/iconButton.js
+++ frontend/src/template/iconButton.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 export default props => {
+    const isHide = props.hide
     if(props.hide) {
         return null
     } else {
         return (
-            <button className={'btn btn-'+props.style} onClick={props.onClick}>
-                <i className={'fa fa-'+props.icon}></i>
-            </button>
+            <div>
+                {isHide ? (
+                    ''
+                ) : (
+                    <button className={'btn btn-'+props.style} onClick={props.onClick}>
+                        <i className={'fa fa-'+props.icon}></i>
+                    </button>
+                )}
+            </div>
         )
     }
 }
