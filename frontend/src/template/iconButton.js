63471c4712d557fa62a05873f4c4a741be7b214d
Abilio Bittencourt Junior <abilio.junior@b2wdigital.com>
2018-03-23 16:07:20 -0300

    Deixando lógica em javascript comum, para acertar posteriormente

--- frontend/src/template/iconButton.js
+++ frontend/src/template/iconButton.js
@@ -2,19 +2,12 @@ import React from 'react'
 
 export default props => {
     const isHide = props.hide
-    if(props.hide) {
+    if(props.hide){
         return null
-    } else {
-        return (
-            <div>
-                {isHide ? (
-                    ''
-                ) : (
-                    <button className={'btn btn-'+props.style} onClick={props.onClick}>
-                        <i className={'fa fa-'+props.icon}></i>
-                    </button>
-                )}
-            </div>
-        )
     }
+    return (
+        <button className={'btn btn-'+props.style} onClick={props.onClick}>
+            <i className={'fa fa-'+props.icon}></i>
+        </button>
+    )
 }
